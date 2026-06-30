c2e7dd8c093b2d31cca900117b6199320cb3e3b0
Dairo_montiel <dairomontiel286@gmail.com>
2026-06-30 16:03:04 -0500

    Ya termiamos

--- PostgrelSQL/find_functions.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  p.proname AS function_name, 
-  pg_get_function_identity_arguments(p.oid) AS arguments
-FROM pg_proc p
-JOIN pg_namespace n ON p.pronamespace = n.oid
-WHERE n.nspname = 'public' 
-  AND p.proname = 'actualizar_liquidacion';
